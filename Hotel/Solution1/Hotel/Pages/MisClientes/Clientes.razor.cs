a558f1336260c8f6f6aa75c2b7595287f5709cff
AxelRodriguez1328 <axelrodriguez201575@gmail.com>
2023-04-28 19:50:32 -0500

    Clientes

--- Hotel/Solution1/Hotel/Pages/MisClientes/Clientes.razor.cs
+++ Hotel/Solution1/Hotel/Pages/MisClientes/Clientes.razor.cs
@@ -1,6 +1,18 @@
-﻿namespace Hotel.Pages.MisClientes
+﻿using Hotel.Interfaces;
+using Microsoft.AspNetCore.Components;
+using Modelos;
+
+namespace Hotel.Pages.MisClientes
 {
-    public class Clientes
+    public partial class Clientes
     {
+        [Inject] private IClienteServicio clienteServicio { get; set; }
+
+        private IEnumerable<Cliente> lista { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            lista = await clienteServicio.GetLista();
+        }
     }
 }
